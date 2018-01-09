6036fecea2624d66199be3dcf5be7e8df219a42d
zsh <zhusonghua0515@163.com>
2018-01-09 14:52:04 +0800

    初始化

--- mis-uaa/src/main/java/com/zsh/mis/misuaa/MisUaaApplication.java
+++ mis-uaa/src/main/java/com/zsh/mis/misuaa/MisUaaApplication.java
@@ -1,16 +1,14 @@
-package com.zsh.mis.misconfig;
+package com.zsh.mis.misuaa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableDiscoveryClient
-@EnableConfigServer
 @SpringBootApplication
-public class MisConfigApplication {
+public class MisUaaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MisConfigApplication.class, args);
+		SpringApplication.run(MisUaaApplication.class, args);
 	}
 }
