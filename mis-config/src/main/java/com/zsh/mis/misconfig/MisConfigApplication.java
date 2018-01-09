d539e2050bb0f7ac76f4771c2ef1e830b2c6b479
zsh <zhusonghua0515@163.com>
2018-01-09 10:53:05 +0800

    初始化

---
+++ mis-config/src/main/java/com/zsh/mis/misconfig/MisConfigApplication.java
@@ -0,0 +1,12 @@
+package com.zsh.mis.misconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisConfigApplication.class, args);
+	}
+}
---
+++ mis-server/src/main/java/com/zsh/mis/misserver/MisServerApplication.java
@@ -0,0 +1,12 @@
+package com.zsh.mis.misserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisServerApplication.class, args);
+	}
+}
